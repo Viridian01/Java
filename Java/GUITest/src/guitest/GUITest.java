e6484a6bbd865f712d4313267d047a1dd60f1908
Viridian01 <nicksnak01@gmail.com>
2016-07-28 23:27:07 -0700

    Added Java workspace for NetBeans

---
+++ Java/GUITest/src/guitest/GUITest.java
@@ -0,0 +1,11 @@
+package guitest;
+
+public class GUITest {
+
+    public static void main(String[] args) 
+    {
+        GUIFrame frame = new GUIFrame();
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    }
+    
+}
---
+++ Java/Test/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JDialog;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		JDialog win = new JDialog();
+
+	}
+
+}
